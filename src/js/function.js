00ce7d2ee0721b45b67675728b13155634336959
Ryan Aristosa <ryanaristosa@outlook.com>
2022-05-15 18:32:53 +0800

    Revamped website

---
+++ src/js/function.js
@@ -0,0 +1,24 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector("nav ul");
+const links = document.querySelectorAll("nav ul li");
+
+hamburger.addEventListener('click', ()=>{
+    navLinks.classList.toggle("nav-open");
+
+    links.forEach(link => {
+        link.classList.toggle("nav-fade");
+    });
+
+    hamburger.classList.toggle("nav-toggle");
+    
+});
+
+navLinks.addEventListener('click', ()=>{
+    navLinks.classList.toggle("nav-open");
+
+    links.forEach(link => {
+        link.classList.toggle("nav-fade");
+    });
+
+    hamburger.classList.toggle("nav-toggle");
+});
