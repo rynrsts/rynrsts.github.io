82ceaf9f49386df182f5b507c6fffb065f866e91
Ryan Aristosa <ryanaristosa@outlook.com>
2022-06-02 12:31:02 +0800

    Convert indention to tabs

--- function.js
+++ function.js
@@ -3,22 +3,22 @@ const navLinks = document.querySelector("nav ul");
 const links = document.querySelectorAll("nav ul li");
 
 hamburger.addEventListener('click', ()=>{
-    navLinks.classList.toggle("nav-open");
+	navLinks.classList.toggle("nav-open");
 
-    links.forEach(link => {
-        link.classList.toggle("nav-fade");
-    });
+	links.forEach(link => {
+		link.classList.toggle("nav-fade");
+	});
 
-    hamburger.classList.toggle("nav-toggle");
-    
+	hamburger.classList.toggle("nav-toggle");
+	
 });
 
 navLinks.addEventListener('click', ()=>{
-    navLinks.classList.toggle("nav-open");
+	navLinks.classList.toggle("nav-open");
 
-    links.forEach(link => {
-        link.classList.toggle("nav-fade");
-    });
+	links.forEach(link => {
+		link.classList.toggle("nav-fade");
+	});
 
-    hamburger.classList.toggle("nav-toggle");
+	hamburger.classList.toggle("nav-toggle");
 });
